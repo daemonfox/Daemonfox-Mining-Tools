0ef322ef30198e7ad3e5ddcc60be66f9430a1f7c
daemonfox <bdavistech@gmail.com>
2020-04-13 23:04:35 -0400

    New files for VOTE2020

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
 # Daemonfox-Mining-Tools
-CMDlet for POS64 KMD based coins.
+CMDlet for KMD based coins.
 
-Copy this file to the folder where the daemon files reside. Typically for AGAMA, this is in the main folder you launch the shortcut, but in this directory:
-\resources\app\assets\bin\win64
+CHANGE LOG:
 
-If running the CLI daemon, this goes in the same folder you launched the daemon from.
+1. Added new batch file to launch VOTE2020 chain in cmd window using Verus Desktop daemon
+	A. Launch VOTE2020 with the press of a button and your existing VerusDesktop wallet or AGAMA!!!
+	B. Can be launched manually or fromt he CMDlet
+2. Added a new CMDlet to interact with VOTE2020 without a grpahic UI.
+	A. Launches VOTE2020 in a new cmd window
+	B. Can display wallet status
+	C. Can import your privkey
+	D. Can display your imported balance
+	E. Preconfigured option to VOTE FOR DAEMONFOX with any amount owned
+	F. MEthod to vote for any NN address included
+
+ALL FILES ARE IN PLAIN TEXT AND CAN BE COPY PASTED INSTEAD OF DOWNLOADED. COMPLETE TRANSPARAENCY, USES 100% WINDOWS CMD AND KOMODO BINARIES.
+
+Copy these file to the main wallet folder. Typically for Verus Desktop, this is in the C:\Program Files\Verus-Desktop folder.
+
+An easy way to get to this is to use the Debug menu in Verus Desktop and click the last option, Show binary folder, then browse up to the main directory.
+
+If running the CLI daemon, these go in the same folder you launch the Komodo daemon from.
 
 Suggestions are welcome. I can be found on Discord daemonfox#6136 and in the KMDLABS channel. https://discord.gg/HHzJbMC
 
-KMD love is appreciated! RFeZSx2h7yEaXaq5PWLEHgAQYTp9PqK4CF
+VOTE FOR DAEMONFOX RDAEmonF9avm7xau2pYWf3UamgHNXrkzpt
+
+KMD love is appreciated!
---
+++ VOTE2020_DFMTools.bat
@@ -0,0 +1,106 @@
+@call :GET_CURRENT_DIR
+@cd %THIS_DIR%
+ECHO OFF
+CLS
+:MENU
+CLS
+ECHO.
+ECHO           Daemonfox Mining Toolbox     
+ECHO VOTE FOR DAEMONFOX RDAEmonF9avm7xau2pYWf3UamgHNXrkzpt
+ECHO. 
+ECHO .....................................................
+ECHO PRESS 1-9 to select your task, or q to EXIT.
+ECHO .....................................................
+ECHO.
+ECHO 1 - START VOTE2020
+ECHO 2 - CHECK GETINFO
+ECHO 3 - IMPORTPRIVKEY
+ECHO 4 - CHECK GETWALLETINFO
+ECHO 5 - VOTE FOR DAEMONFOX
+ECHO 6 - VOTE FOR ANY NN
+ECHO q - EXIT
+ECHO.
+color 9
+SET /P M=Type 1, 2, 3, 4, 5, 6 or q then press ENTER:
+IF %M%==1 GOTO VOTE2020
+IF %M%==2 GOTO GETINFO
+IF %M%==3 GOTO IMPORTPRIVKEY
+IF %M%==4 GOTO GETWALLETINFO
+IF %M%==5 GOTO DAEMONFOX
+IF %M%==6 GOTO NN
+IF %M%==q GOTO EOF
+
+:VOTE2020
+@call :GET_CURRENT_DIR
+@cd %THIS_DIR%
+CLS
+cd resources\app\assets\bin\win64\komodod
+start VOTE2020.bat
+ECHO.
+ECHO VOTE2020 STARTED VOTE FOR DAEMONFOX RDAEmonF9avm7xau2pYWf3UamgHNXrkzpt
+TIMEOUT /T 20
+GOTO MENU
+
+:GETINFO
+@call :GET_CURRENT_DIR
+@cd %THIS_DIR%
+CLS
+cd resources\app\assets\bin\win64\komodod
+komodo-cli -ac_name=VOTE2020 getinfo 
+ECHO.
+ECHO VOTE FOR DAEMONFOX RDAEmonF9avm7xau2pYWf3UamgHNXrkzpt
+TIMEOUT /T 20
+GOTO MENU
+
+:IMPORTPRIVKEY
+@call :GET_CURRENT_DIR
+@cd %THIS_DIR%
+CLS
+cd resources\app\assets\bin\win64\komodod
+set /p privkey=Enter PrivKey: 
+komodo-cli -ac_name=VOTE2020 importprivkey %privkey%
+ECHO.
+ECHO Key imported. VOTE FOR DAEMONFOX RDAEmonF9avm7xau2pYWf3UamgHNXrkzpt
+TIMEOUT /T 20
+GOTO MENU
+
+:GETWALLETINFO
+@call :GET_CURRENT_DIR
+@cd %THIS_DIR%
+CLS
+cd resources\app\assets\bin\win64\komodod
+komodo-cli -ac_name=VOTE2020 getwalletinfo
+ECHO.
+ECHO VOTE FOR DAEMONFOX RDAEmonF9avm7xau2pYWf3UamgHNXrkzpt
+TIMEOUT /T 20
+GOTO MENU
+
+:DAEMONFOX
+@call :GET_CURRENT_DIR
+@cd %THIS_DIR%
+CLS
+cd resources\app\assets\bin\win64\komodod
+set /p amt=Enter VOTE2020 amount to send: 
+komodo-cli -ac_name=VOTE2020 sendtoaddress RDAEmonF9avm7xau2pYWf3UamgHNXrkzpt %amt%
+ECHO.
+ECHO THANK YOU!! THANK YOU!! THANK YOU!!
+TIMEOUT /T 20
+GOTO MENU
+
+:NN
+@call :GET_CURRENT_DIR
+@cd %THIS_DIR%
+CLS
+cd resources\app\assets\bin\win64\komodod
+set /p addr=Enter NN Address: 
+set /p amt=Enter VOTE2020 amount to send: 
+komodo-cli -ac_name=VOTE2020 sendtoaddress %addr% %amt%
+ECHO.
+ECHO VOTE FOR DAEMONFOX RDAEmonF9avm7xau2pYWf3UamgHNXrkzpt
+TIMEOUT /T 15
+GOTO MENU
+
+:GET_CURRENT_DIR
+@pushd %~dp0
+@set THIS_DIR=%CD%
+@popd
