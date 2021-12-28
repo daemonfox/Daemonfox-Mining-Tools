e7d0857fbe9fb6c6ccf0300dba417573059906e5
daemonfox <bdavistech@gmail.com>
2021-12-28 12:42:07 -0500

    Add files via upload

---
+++ tokel.bat
@@ -0,0 +1,1 @@
+komodod.exe -ac_name=TOKEL -ac_supply=100000000 -ac_eras=2 -ac_cbmaturity=1 -ac_reward=100000000,4250000000 -ac_end=80640,0 -ac_decay=0,77700000 -ac_halving=0,525600 -ac_cc=555 -ac_ccenable=236,245,246,247 -ac_adaptivepow=6 -addnode=135.125.204.169 -addnode=192.99.71.125 -addnode=144.76.140.197 -addnode=135.181.92.123
